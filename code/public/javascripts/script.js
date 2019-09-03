3ec1d2323516506f8bf3544fa5610640d47e03d6
bkauf@google.com <bkauf@google.com>
2019-09-03 10:01:09 -0400

    link changes

--- code/public/javascripts/script.js
+++ code/public/javascripts/script.js
@@ -1,12 +1,12 @@
 $(document).ready( function() {
   $(".health").click(function(e){
-    $.get("/health", function(data, status){
+    $.get("health", function(data, status){
         $('#status').text('Health:'+data);
       });
           //alert("Data: " + data + "\nStatus: " + status);
     });
   $(".kill").click(function(e){
-    $.get("/kill", function(data, status){
+    $.get("kill", function(data, status){
       $('#status').text('Health:'+data);
           //  alert("Data: " + data + "\nStatus: " + status);
         });
